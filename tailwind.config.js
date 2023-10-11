1e669d880c0630d350d7ceeda52ea9a90a0446e6
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-11 16:25:43 -0300

    Add orange button color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange-button": "#ff561e",
+      },
+    },
   },
   plugins: [],
 };
