f42a31c777682a2bd538d3929a49fede9697551a
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-11 17:47:47 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { Provider } from "react-redux";
 
 import Error404 from "containers/errors/Error404";
 import Home from "containers/pages/Home";
+import Cases from "containers/pages/Cases";
+import Services from "containers/pages/Services";
+import About from "containers/pages/About";
+import Careers from "containers/pages/Careers";
+import Blog from "containers/pages/Blog";
+import Contact from "containers/pages/Contact";
 
 function App() {
   return (
@@ -15,6 +21,12 @@ function App() {
 
           {/* Home Display */}
           <Route path="/" element={<Home />} />
+          <Route path="/cases" element={<Cases />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </Provider>
