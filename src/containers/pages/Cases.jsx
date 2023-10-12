c1db508ebe3ef65ca5bb754a288d6e64bf9dddcc
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-12 17:26:47 -0300

    Add Cases sections

--- src/containers/pages/Cases.jsx
+++ src/containers/pages/Cases.jsx
@@ -1,3 +1,5 @@
+import CasesList from "components/cases/CasesList";
+import Header from "components/cases/Header";
 import Footer from "components/navigation/Footer";
 import Navbar from "components/navigation/Navbar";
 import Layout from "hocs/layouts/Layout";
@@ -6,7 +8,10 @@ function Cases() {
   return (
     <Layout>
       <Navbar />
-      <div className="pt-28">Cases</div>
+      <div className="pt-28">
+        <Header />
+        <CasesList />
+      </div>
       <Footer />
     </Layout>
   );
