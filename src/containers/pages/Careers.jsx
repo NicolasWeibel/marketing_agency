3eb46c0969d6e2d04f05b444b0138fbe15068c64
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-13 02:20:42 -0300

    Add Careers sections

--- src/containers/pages/Careers.jsx
+++ src/containers/pages/Careers.jsx
@@ -1,3 +1,8 @@
+import Features from "components/careers/Features";
+import Header from "components/careers/Header";
+import LogoCloud from "components/careers/LogoCloud";
+import PositionsList from "components/careers/PositionsList";
+import Testimonial from "components/careers/Testimonial";
 import Footer from "components/navigation/Footer";
 import Navbar from "components/navigation/Navbar";
 import Layout from "hocs/layouts/Layout";
@@ -10,7 +15,14 @@ function Careers() {
   return (
     <Layout>
       <Navbar />
-      <div className="pt-28">Careers</div>
+      <div className="pt-28">
+        <Header />
+        <Testimonial />
+        <LogoCloud />
+        <Features />
+        <Features />
+        <PositionsList />
+      </div>
       <Footer />
     </Layout>
   );
