ea8832034c3cd1d8f058b6468f4fbc61a1fe23af
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-13 03:54:53 -0300

    Add transitions between pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import store from "store";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./store";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 
-import Error404 from "containers/errors/Error404";
-import Home from "containers/pages/Home";
-import Cases from "containers/pages/Cases";
-import Services from "containers/pages/Services";
-import About from "containers/pages/About";
-import Careers from "containers/pages/Careers";
-import Blog from "containers/pages/Blog";
-import Contact from "containers/pages/Contact";
+import AnimatedRoutes from "Routes";
 
 function App() {
   return (
@@ -55,19 +48,7 @@ function App() {
       </Helmet>
       <Provider store={store}>
         <Router>
-          <Routes>
-            {/* Error Display */}
-            <Route path="*" element={<Error404 />} />
-
-            {/* Home Display */}
-            <Route path="/" element={<Home />} />
-            <Route path="/cases" element={<Cases />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/about-us" element={<About />} />
-            <Route path="/careers" element={<Careers />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+          <AnimatedRoutes />
         </Router>
       </Provider>
     </HelmetProvider>
