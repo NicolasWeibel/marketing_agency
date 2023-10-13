63014e3c7ecb96b356c04526721f16c8da7ad80a
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-10-13 01:59:39 -0300

    Add About Us sections

--- src/containers/pages/About.jsx
+++ src/containers/pages/About.jsx
@@ -1,3 +1,10 @@
+import CTA from "components/about/CTA";
+import Clients from "components/about/Clients";
+import Features from "components/about/Features";
+import Header from "components/about/Header";
+import Images from "components/about/Images";
+import Team from "components/about/Team";
+import TestStats from "components/about/TestStats";
 import Footer from "components/navigation/Footer";
 import Navbar from "components/navigation/Navbar";
 import Layout from "hocs/layouts/Layout";
@@ -11,7 +18,56 @@ function About() {
   return (
     <Layout>
       <Navbar />
-      <div className="pt-28">About</div>
+      <div className="pt-28">
+        <Header />
+        <TestStats />
+        <Images />
+        <Clients />
+        <div className="bg-white">
+          <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
+            <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
+              <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+                  alt="Tuple"
+                />
+              </div>
+              <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
+                  alt="Mirage"
+                />
+              </div>
+              <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+                  alt="StaticKit"
+                />
+              </div>
+              <div className="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+                  alt="Transistor"
+                />
+              </div>
+              <div className="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+                <img
+                  className="h-12"
+                  src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
+                  alt="Workcation"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <Features />
+        <Team />
+        <CTA />
+      </div>
       <Footer />
     </Layout>
   );
